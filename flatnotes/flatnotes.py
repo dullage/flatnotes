627f36a0951c1ac98778f7cf0d8c3ff86228dc21
Gedulis12 <80179975+Gedulis12@users.noreply.github.com>
2023-08-07 21:38:06 +0300

    Revert "Ignore markdown codeblocks for tags (#66)"
    This reverts commit f7d206c0f3603bdf692f03cbd038a5666d9aa84b.

--- flatnotes/flatnotes.py
+++ flatnotes/flatnotes.py
@@ -20,7 +20,7 @@ from helpers import empty_dir, re_extract, strip_ext
 from logger import logger
 
 MARKDOWN_EXT = ".md"
-INDEX_SCHEMA_VERSION = "4"
+INDEX_SCHEMA_VERSION = "3"
 
 StemmingFoldingAnalyzer = StemmingAnalyzer() | CharsetFilter(accent_map)
 
@@ -170,7 +170,6 @@ class SearchResult(Note):
 
 class Flatnotes(object):
     TAGS_RE = re.compile(r"(?:(?<=^#)|(?<=\s#))\w+(?=\s|$)")
-    CODEBLOCK_RE = re.compile(r"`{1,3}.*?`{1,3}", re.DOTALL)
     TAGS_WITH_HASH_RE = re.compile(r"(?:(?<=^)|(?<=\s))#\w+(?=\s|$)")
 
     def __init__(self, dir: str) -> None:
@@ -212,9 +211,7 @@ class Flatnotes(object):
 
         - The content without the tags.
         - A set of tags converted to lowercase."""
-        content_ex_codeblock = re.sub(cls.CODEBLOCK_RE, '', content)
-        _, tags = re_extract(cls.TAGS_RE, content_ex_codeblock)
-        content_ex_tags, _ = re_extract(cls.TAGS_RE, content)
+        content_ex_tags, tags = re_extract(cls.TAGS_RE, content)
         try:
             tags = [tag.lower() for tag in tags]
             return (content_ex_tags, set(tags))
