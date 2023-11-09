0e1fd8ab76f7b11e10686e7056c4c34ca935d99b
Adam Dullage <adam@dullage.com>
2023-11-09 19:20:24 +0000

    Removed unnecessary css import

--- flatnotes/src/components/NoteViewerEditor.vue
+++ flatnotes/src/components/NoteViewerEditor.vue
@@ -129,7 +129,6 @@
 <style lang="scss">
 // Toast UI Markdown Editor
 @import "@toast-ui/editor/dist/toastui-editor.css";
-@import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 @import "prismjs/themes/prism.css";
 @import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
 
