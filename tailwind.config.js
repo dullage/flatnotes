635d0d3b3e15e9c0e294ecafd9164bcf8b9ea910
Adam Dullage <adam@dullage.com>
2024-04-23 20:16:05 +0100

    Theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["client/**/*.{html,js,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-brand": "rgb(var(--theme-brand) / <alpha-value>)",
+        "theme-background": "rgb(var(--theme-background) / <alpha-value>)",
+        "theme-background-elevated": "rgb(var(--theme-background-elevated) / <alpha-value>)",
+        "theme-background-tint": "rgb(var(--theme-background-tint) / <alpha-value>)",
+        "theme-background-highlight": "rgb(var(--theme-background-highlight) / <alpha-value>)",
+        "theme-text": "rgb(var(--theme-text) / <alpha-value>)",
+        "theme-text-muted": "rgb(var(--theme-text-muted) / <alpha-value>)",
+        "theme-text-very-muted": "rgb(var(--theme-text-very-muted) / <alpha-value>)",
+        "theme-shadow": "rgb(var(--theme-shadow) / <alpha-value>)",
+        "theme-border": "rgb(var(--theme-border) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
