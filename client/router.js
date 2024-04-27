5d684b352366675979fa577a6ab90703d7753f34
Adam Dullage <adam@dullage.com>
2024-04-27 16:35:30 +0100

    Dynamic route imports

--- client/router.js
+++ client/router.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from "/views/Home.vue";
-import LogIn from "/views/LogIn.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home,
+      component: () => import("./views/Home.vue"),
     },
     {
       path: "/login",
       name: "login",
-      component: LogIn,
+      component: () => import("./views/LogIn.vue"),
     },
   ],
 });
