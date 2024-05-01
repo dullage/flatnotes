23dcc296a9acd18b83c22e0c028f59553da2b8ec
Adam Dullage <adam@dullage.com>
2024-05-01 21:46:31 +0100

    Remove incorrect rounded-r-none class from TextInput

--- client/components/TextInput.vue
+++ client/components/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     type="text"
-    class="w-full rounded rounded-r-none border border-theme-border bg-theme-background-elevated px-3 py-2 focus:outline-none focus:ring-1"
+    class="w-full rounded border border-theme-border bg-theme-background-elevated px-3 py-2 focus:outline-none focus:ring-1"
     placeholder="Search"
     v-model="model"
   />
