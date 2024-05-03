91a6c89a424f9dae9b718606941d10c13a0ce78a
Adam Dullage <adam@dullage.com>
2024-05-03 11:55:21 +0100

    Add ToastViewer component

---
+++ client/components/Toast/ToastViewer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="viewerElement"></div>
+</template>
+
+<script setup>
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js";
+import Viewer from "@toast-ui/editor/dist/toastui-editor-viewer";
+import { onMounted, ref } from "vue";
+
+const props = defineProps({
+  initialValue: String,
+});
+
+const viewerElement = ref();
+
+onMounted(() => {
+  new Viewer({
+    el: viewerElement.value,
+    initialValue: props.initialValue,
+    plugins: [codeSyntaxHighlight],
+  });
+});
+</script>
+
+<style>
+@import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+@import "prismjs/themes/prism.css";
+@import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+@import "./toastui-editor-overrides.scss";
+</style>
