c724d06edeb7fd95eec9be14e15ad44349423eff
Adam Dullage <adam@dullage.com>
2024-05-04 11:52:30 +0100

    Add exposed focus function to TextInput component

--- client/components/TextInput.vue
+++ client/components/TextInput.vue
@@ -4,9 +4,19 @@
     class="w-full rounded border border-theme-border bg-theme-background-elevated px-3 py-2 focus:outline-none focus:ring-1"
     placeholder="Search"
     v-model="model"
+    ref="input"
   />
 </template>
 
 <script setup>
-const model = defineModel()
+import { ref } from "vue";
+
+const model = defineModel();
+const input = ref();
+
+function focus() {
+  input.value.focus();
+}
+
+defineExpose({ focus });
 </script>
